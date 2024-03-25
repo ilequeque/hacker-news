68a9f8e0946e5eff17ad369bc1d37ba76625b82c
ilequeque <ilyas.shakhabdinov@gmail.com>
2024-03-26 03:17:32 +0500

    Initial Commit

---
+++ hacker news/hacker_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hacker_newsApp.swift
+//  hacker news
+//
+//  Created by Iliyas Shakhabdin on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct hacker_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
